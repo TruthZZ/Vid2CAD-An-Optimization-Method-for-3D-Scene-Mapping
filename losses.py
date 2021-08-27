7b1f96bac8a26c5f25dab11044a668c5803c3de8
Zhenli <42024221+TruthZZ@users.noreply.github.com>
2021-08-27 18:01:54 +0800

    Add files via upload

---
+++ losses.py
@@ -0,0 +1,147 @@
+import torch
+import torch.nn as nn
+
+class OverallLoss(nn.Module):
+    def __init__(self):
+        super(OverallLoss, self).__init__()
+
+        self.pixel_center_loss = Pixel_Center_Loss()
+        self.world_center_loss = World_Center_Loss()
+        self.rotation_loss = Rotation_Loss()
+        self.scale_bbox_loss = Scale_Bbox_Loss()
+        self.scale_class_loss = Scale_Class_Loss()
+
+        self.coefficients = [0.2, 0.2, 0.2, 0.2, 0.2]
+
+    def forward(self, Rotation, center, bbox, bbox_3d, ER, et, K, kx, ky, beta, t, R, s, s_cls):
+        overall_loss = torch.tensor([0], dtype=torch.float32)
+
+        k = torch.cat((kx, ky))
+
+        R = R / torch.norm(R)
+        R_mat = [[],[],[]]
+        R_mat[0].append((1-2*(torch.square(R[1])+torch.square(R[2]))).unsqueeze(0))
+        R_mat[0].append((2*(R[0]*R[1]-R[2]*R[3])).unsqueeze(0))
+        R_mat[0].append((2*(R[0]*R[2]+R[1]*R[3])).unsqueeze(0))
+        R_mat[1].append((2*(R[0]*R[1]+R[2]*R[3])).unsqueeze(0))
+        R_mat[1].append((1-2*(torch.square(R[0])+torch.square(R[2]))).unsqueeze(0))
+        R_mat[1].append((2*(R[1]*R[2]-R[0]*R[3])).unsqueeze(0))
+        R_mat[2].append((2*(R[0]*R[2]-R[1]*R[3])).unsqueeze(0))
+        R_mat[2].append((2*(R[1]*R[2]+R[0]*R[3])).unsqueeze(0))
+        R_mat[2].append((1-2*(torch.square(R[0])+torch.square(R[1]))).unsqueeze(0))
+
+        R_mat[0] = torch.cat(R_mat[0]).unsqueeze(0)
+        R_mat[1] = torch.cat(R_mat[1]).unsqueeze(0)
+        R_mat[2] = torch.cat(R_mat[2]).unsqueeze(0)
+        R_mat = torch.cat(R_mat, dim=0).float()
+
+        extrinsics = torch.cat([ER, et.unsqueeze(0)], dim=0)
+        temp = torch.tensor([0,0,0,1]).unsqueeze(0).t()
+        extrinsics = torch.cat([extrinsics, temp], dim=1)
+        extrinsics_inv = torch.linalg.inv(extrinsics)
+        bbox_3d_corners = [
+            torch.tensor([bbox_3d[0], bbox_3d[2], bbox_3d[4]]),
+            torch.tensor([bbox_3d[0], bbox_3d[2], bbox_3d[5]]),
+            torch.tensor([bbox_3d[0], bbox_3d[3], bbox_3d[4]]),
+            torch.tensor([bbox_3d[0], bbox_3d[3], bbox_3d[5]]),
+            torch.tensor([bbox_3d[1], bbox_3d[2], bbox_3d[4]]),
+            torch.tensor([bbox_3d[1], bbox_3d[2], bbox_3d[5]]),
+            torch.tensor([bbox_3d[1], bbox_3d[3], bbox_3d[4]]),
+            torch.tensor([bbox_3d[1], bbox_3d[3], bbox_3d[5]])
+        ]
+        for i in range(8):
+            bbox_3d_corners[i] = torch.mul(s.t(), bbox_3d_corners[i])
+            bbox_3d_corners[i] = bbox_3d_corners[i].t().squeeze()
+            bbox_3d_corners[i] = torch.mv(R_mat, bbox_3d_corners[i]).unsqueeze(0).t()
+            bbox_3d_corners[i] += t
+        for i in range(8):
+            bbox_3d_corners[i] = torch.cat([bbox_3d_corners[i], torch.tensor([1]).unsqueeze(0)])
+            bbox_3d_corners[i] = torch.mm(bbox_3d_corners[i].t(), extrinsics_inv)
+            bbox_3d_corners[i] = bbox_3d_corners[i].t()[:3]
+            bbox_3d_corners[i] = bbox_3d_corners[i] / bbox_3d_corners[i][2]
+            bbox_3d_corners[i] = torch.mm(K, bbox_3d_corners[i]).t()
+        bbox_3d_corners = torch.cat(bbox_3d_corners)
+        mins = torch.min(bbox_3d_corners, dim=0)
+        maxs = torch.max(bbox_3d_corners, dim=0)
+        bbox_2d = torch.cat([mins[0][0].unsqueeze(0)/960,
+                            maxs[0][0].unsqueeze(0)/960,
+                            mins[0][1].unsqueeze(0)/720,
+                            maxs[0][1].unsqueeze(0)/720])
+
+        pixel_center_loss = self.pixel_center_loss(k, center)
+        world_center_loss = self.world_center_loss(kx, ky, beta, ER, et, K, t)
+        rotation_loss = self.rotation_loss(Rotation, ER, R_mat)
+        scale_bbox_loss = self.scale_bbox_loss(bbox_2d, bbox)
+        scale_cls_loss = self.scale_class_loss(s, s_cls)
+
+        # print("pixel_center_loss:", pixel_center_loss)
+        # print("world_center_loss:", world_center_loss)
+        # print("rotation_loss:", rotation_loss)
+        # print("scale_bbox_loss:", scale_bbox_loss)
+        # print("scale_cls_loss:", scale_cls_loss)
+
+        overall_loss += self.coefficients[0] * pixel_center_loss
+        overall_loss += self.coefficients[1] * world_center_loss
+        overall_loss += self.coefficients[2] * rotation_loss
+        overall_loss += self.coefficients[3] * scale_bbox_loss
+        overall_loss += self.coefficients[4] * scale_cls_loss
+
+        return [overall_loss, pixel_center_loss, world_center_loss, rotation_loss, scale_bbox_loss, scale_cls_loss]
+
+class Pixel_Center_Loss(nn.Module):
+    def __init__(self):
+        super(Pixel_Center_Loss, self).__init__()
+        self.l1loss = nn.L1Loss(reduction='mean')
+
+    def forward(self, k, c):
+
+        return self.l1loss(k, c)
+
+class World_Center_Loss(nn.Module):
+    def __init__(self):
+        super(World_Center_Loss, self).__init__()
+        self.l1loss = nn.L1Loss(reduction='mean')
+
+    def forward(self, kx, ky, beta, ER, et, K, t):
+        # camera_coors = torch.tensor([[beta*kx*960, beta*ky*720, beta]], requires_grad=True).t()
+        camera_coors = torch.cat([beta*kx*960, beta*ky*720, beta], dim=0).unsqueeze(0).t()
+        # print(beta, kx, ky)
+        # print(camera_coors)
+        camera_coors = torch.mm(torch.linalg.inv(K), camera_coors)
+        l2wMat = torch.cat([ER, et.unsqueeze(0)], dim=0)
+        temp = torch.tensor([0,0,0,1]).unsqueeze(0).t()
+        l2wMat = torch.cat([l2wMat, temp], dim=1)
+        camera_coors = torch.cat([camera_coors, torch.tensor([1]).unsqueeze(0)])
+        world_coors = torch.mm(camera_coors.t(), l2wMat).squeeze()[:3]
+        # print(world_coors, t.t().squeeze())
+        loss = self.l1loss(world_coors, t.t().squeeze())
+
+        return loss
+
+class Rotation_Loss(nn.Module):
+    def __init__(self):
+        super(Rotation_Loss, self).__init__()
+        self.l2loss = nn.MSELoss(reduction='mean')
+
+    def forward(self, Ri, ER, R_mat):
+        loss1 = self.l2loss(Ri, torch.mm(ER, R_mat))
+        
+        return loss1
+
+class Scale_Bbox_Loss(nn.Module):
+    def __init__(self):
+        super(Scale_Bbox_Loss, self).__init__()
+        self.l1loss = nn.L1Loss(reduction='mean')
+
+    def forward(self, bbox_v, bbox_i):
+        
+        return self.l1loss(bbox_v, bbox_i)
+
+class Scale_Class_Loss(nn.Module):
+    def __init__(self):
+        super(Scale_Class_Loss, self).__init__()
+        self.l1loss = nn.L1Loss(reduction='mean')
+
+    def forward(self, scale_v, scale_c):
+
+        return self.l1loss(scale_v.squeeze(), scale_c)
---
+++ optimizer.py
@@ -0,0 +1,163 @@
+from posix import listdir
+import torch
+import torch.nn as nn
+from torch.nn.modules.module import register_module_backward_hook
+from losses import OverallLoss
+import os
+import json
+import torch.optim as optim
+from torch.autograd import Variable
+from scipy.spatial.transform import Rotation as sRota
+
+class optimizer():
+    def __init__(self):
+        super(optimizer, self).__init__()
+
+        self.big_epoch_num = 5000
+        self.lr = 0.002
+        self.loss_func = OverallLoss()
+
+        path_frame = './Syn_data/frames/'
+        path_jsons = './Syn_data/jittered/'
+        listdir_frame = os.listdir(path_frame)
+        listdir_jsons = os.listdir(path_jsons)
+        self.objects_frame_dict = {}
+        self.frame_num = len(listdir_frame)
+
+        self.all_frame_dict = {}
+        for i in range(self.frame_num):
+            tmp_path = path_jsons + listdir_jsons[i]
+            with open(tmp_path, 'r') as f:
+                frame_dict = json.load(f)
+            self.all_frame_dict[i] = frame_dict
+
+            frame_objects = frame_dict['objects'].keys()
+            for keys in frame_objects:
+                if keys not in self.objects_frame_dict.keys():
+                    self.objects_frame_dict[keys] = []
+                obj_info_dict = frame_dict['objects'][keys]
+                pixel_coors = []
+                pixel_coors += obj_info_dict['center']
+                pixel_coors += obj_info_dict['bounding_box_2d']
+                flag = True
+                for pc in pixel_coors:
+                    if pc < 0:
+                        flag = False
+                if pixel_coors[0] > 959 or pixel_coors[2] > 959 or pixel_coors[3] > 959:
+                    flag = False
+                if pixel_coors[1] > 719 or pixel_coors[4] > 719 or pixel_coors[5] > 719:
+                    flag = False
+                if flag:
+                    self.objects_frame_dict[keys].append(i)
+
+        # self.res_list = []
+        # self.all_loss_list = []
+        self.overall_loss = {}
+
+    def optm_single_obj(self, obj_name):
+        self.overall_loss[obj_name] = []
+        frame_list = self.objects_frame_dict[obj_name]
+        t = Variable(torch.randn((3, 1)), requires_grad = True)
+        R = Variable(torch.randn((4)), requires_grad = True)
+        s = Variable(torch.rand(3, 1), requires_grad = True)
+        parameters_list = [t, R, s]
+
+        frame_auxiliary_dict = {}
+        for idx in frame_list:
+            temp_dict = {}
+            temp_dict['kx'] = Variable(torch.rand(1), requires_grad=True)
+            temp_dict['ky'] = Variable(torch.rand(1), requires_grad=True)
+            temp_dict['beta'] = Variable(torch.rand(1), requires_grad=True)
+            frame_auxiliary_dict[idx] = temp_dict
+            parameters_list.append(frame_auxiliary_dict[idx]['kx'])
+            parameters_list.append(frame_auxiliary_dict[idx]['ky'])
+            parameters_list.append(frame_auxiliary_dict[idx]['beta'])
+
+        optimizer_1 = optim.Adam(parameters_list, lr=self.lr)
+
+        for big_epoch in range(self.big_epoch_num):
+            optimizer_1.zero_grad()
+            loss = torch.tensor([0.], requires_grad=True)
+            loss_by_frame = []
+            for frame_idx in frame_list:
+                frame_dict = self.all_frame_dict[frame_idx]
+                K = Variable(torch.tensor(frame_dict['K']))
+                et = Variable(torch.tensor(frame_dict['et']))
+                ER = Variable(torch.tensor(frame_dict['ER']))
+
+                kx = frame_auxiliary_dict[frame_idx]['kx']
+                ky = frame_auxiliary_dict[frame_idx]['ky']
+                beta = frame_auxiliary_dict[frame_idx]['beta']
+
+                obj_info_dict = frame_dict['objects'][obj_name]
+                bbox_3d = obj_info_dict['bounding_box_3d']
+                center_x = obj_info_dict['center'][0] / 960
+                center_y = obj_info_dict['center'][1] / 720
+                center = Variable(torch.tensor([center_x, center_y]))
+                bbox_x_min = obj_info_dict['bounding_box_2d'][0] / 960
+                bbox_x_max = obj_info_dict['bounding_box_2d'][1] / 960
+                bbox_y_min = obj_info_dict['bounding_box_2d'][2] / 720
+                bbox_y_max = obj_info_dict['bounding_box_2d'][3] / 720
+                bbox_2d = Variable(torch.tensor([bbox_x_min, bbox_x_max, bbox_y_min, bbox_y_max]))
+                Rotation = Variable(torch.tensor(obj_info_dict['Rotation']))
+                s_cls = Variable(torch.tensor(obj_info_dict['origin_scale']))
+
+                loss_list = self.loss_func(Rotation, center, bbox_2d,
+                                    bbox_3d,
+                                    ER, et, K,
+                                    kx, ky, beta,
+                                    t, R, s,
+                                    s_cls)
+                loss_by_frame.append([loss_list[0].item(), loss_list[1].item(), loss_list[2].item(), loss_list[3].item(), loss_list[4].item(), loss_list[5].item()])
+                loss = loss + loss_list[0]
+
+            # self.all_loss_list.append(loss_by_frame)
+
+            loss /= len(frame_list)
+            self.overall_loss[obj_name].append(loss.item())
+            loss.backward()
+            optimizer_1.step()
+
+            if big_epoch % 50 == 0:
+                # temp_res_dict = {}
+                # temp_res_dict['t'] = t.tolist()
+                # temp_res_dict['R'] = R.tolist()
+                # temp_res_dict['s'] = s.tolist()
+                # self.res_list.append(temp_res_dict)
+                print("Optimizing Object:", obj_name, "Step:", big_epoch+1, " Loss:", loss.item())
+
+            # if big_epoch == 0 or i == 2:
+            #     self.lr /= 2
+
+        res_dict = {}
+        res_dict['t'] = t.tolist()
+        R = R / torch.norm(R)
+        R = R.tolist()
+        R_scipy = sRota.from_quat(R)
+        R = R_scipy.as_matrix()
+        res_dict['R'] = R.tolist()
+        res_dict['s'] = s.tolist()
+
+        return res_dict
+
+    def optm_all_obj(self):
+        obj_scene_dict = {}
+        for name in self.objects_frame_dict.keys():
+            res_dict = self.optm_single_obj(name)
+            obj_scene_dict[name] = res_dict
+
+        return obj_scene_dict
+
+if __name__ == '__main__':
+    test_optimizer = optimizer()
+    # res_dict = test_optimizer.optm_single_obj('chair_10')
+    # print(res_dict)
+    scene_dict = test_optimizer.optm_all_obj()
+    # with open('./all_res.json', 'w') as f:
+    #     json.dump(test_optimizer.res_list, f, indent=4, sort_keys=False)
+    # with open('./all_loss_list.json', 'w') as f:
+    #     json.dump(test_optimizer.all_loss_list, f, indent=4, sort_keys=False)
+    with open('./scene_objects.json', 'w') as f:
+        json.dump(scene_dict, f, indent=4, sort_keys=False)
+    with open('./overall_loss.json', 'w') as f:
+        json.dump(test_optimizer.overall_loss, f, indent=4, sort_keys=False)
